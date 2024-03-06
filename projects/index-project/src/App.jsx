cc542fa650c4c15a8d4bb57fc27341f23344674c
Alejandro Palacios <srjalean@gmail.com>
2024-03-06 11:11:03 +0100

    estilos del menu
    estilos del menu 'casi' terminado

--- projects/index-project/src/App.jsx
+++ projects/index-project/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import './App.css';
+import './AppMenu.css';
 import { SegundoComponente } from './components/SegundoComponente';
 import { PrimerComponente } from './components/PrimerComponente';
+import OIG13 from './assets/image/OIG13.jpg';
 
 function App() {
   const [paginaActual, setPaginaActual] = useState('menu');
@@ -13,9 +14,12 @@ function App() {
   return (
     <div>
       {paginaActual === 'menu' && (
-        <div>
-          <button onClick={irAPrimerComponente}>Ir a Primer Componente</button>
-          <button onClick={irASegundoComponente}>Ir a Segundo Componente</button>
+        <div className="contenedorImagenes" style={{display: 'flex', height: '100vh', overflow: 'hidden'}} >
+          {/* Imagen para ir al Primer Componente */}
+          <img src={OIG13} alt="Ir a Primer Componente" onClick={irAPrimerComponente} className="imagenInteractiva" />
+
+          {/* Imagen para ir al Segundo Componente */}
+          <img src={OIG13} alt="Ir a Segundo Componente" onClick={irASegundoComponente} className="imagenInteractiva" />
         </div>
       )}
       {paginaActual === 'primerComponente' && <PrimerComponente irAlMenu={irAlMenu} />}
